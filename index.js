e2321c71793ef273f57b00326af22f9da4c6fcee
grant diamond <grant.a.diamond@gmail.com>
2018-03-28 00:23:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function placeOrder(cardNumber) {
   if (cardNumber == undefined){
     return "Sorry, we don't have a credit card on file for you."
   }
+  cart = []
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   // write your code here
 }
