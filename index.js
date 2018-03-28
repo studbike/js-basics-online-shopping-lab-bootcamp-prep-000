a1a7463684ed6d2c781cd1b0a633d8218a3fc1d4
grant diamond <grant.a.diamond@gmail.com>
2018-03-28 00:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function total() {
 
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++)
-  if (item === cart[i].itemName){
+  if (item == cart[i].itemName){
     cart.splice(i, 1)
   }
   else {return "That item is not in your cart."}
