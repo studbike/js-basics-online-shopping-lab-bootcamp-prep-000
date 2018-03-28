3bc936849ba3268e326b00301645127baf18fb5c
grant diamond <grant.a.diamond@gmail.com>
2018-03-28 00:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,5 +49,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  if (cardNumber == false){
+    return "Sorry, we don't have a credit card on file for you."
+  }
+  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   // write your code here
 }
